ad897fc76d7fc18c789600f9c626400cdf606875
h07d0q <46907193+h07d0q@users.noreply.github.com>
2025-08-04 22:37:52 +0200

    Update upgrade.js

--- upgrade.js
+++ upgrade.js
@@ -5,7 +5,8 @@ export const upgradeScripts = [
 			updatedActions: [],
 			updatedFeedbacks: [],
 		}
-		console.log('Upgrading actions:', props.actions)
+		console.log('Upgrade script v2.0.0 running...')
+		// console.log('Upgrading actions:', props.actions)
 		for (const action of props.actions) {
 			// upgrade older actions
 			//if (['routeav', 'otherId'].includes(action.actionId)) { // if multiple actions have to be changed
@@ -16,7 +17,7 @@ export const upgradeScripts = [
 			}
 		}
 
-		console.log('Upgrading feedbacks:', props.feedbacks)
+		// console.log('Upgrading feedbacks:', props.feedbacks)
 		for (const feedback of props.feedbacks) {
 			// upgrade older feedbacks
 			if (feedback.feedbackId === 'input_routing') {
@@ -25,9 +26,74 @@ export const upgradeScripts = [
 				result.updatedFeedbacks.push(feedback)
 			}
 		}
-		console.log('Upgraded Actions:', JSON.stringify(result.updatedActions, null, 2))
-		console.log('Upgraded Feedbacks:', JSON.stringify(result.updatedFeedbacks, null, 2))
+		// console.log('Upgraded Actions:', JSON.stringify(result.updatedActions, null, 2))
+		// console.log('Upgraded Feedbacks:', JSON.stringify(result.updatedFeedbacks, null, 2))
 
+		return result
+	},
+	function v2_1_0(context, props) {
+		const result = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+		console.log('Upgrade script v2.1.0 running...')
+		// console.log('Upgrading config:', props.config)
+		if (props.config.pollInterval === undefined) {
+			props.config.pollInterval = 0
+			result.updatedConfig = props.config
+		}
+		if (props.config.debugLogging === undefined) {
+			props.config.debugLogging = false
+			result.updatedConfig = props.config
+		}
+		// console.log('Upgrading actions:', props.actions)
+		for (const action of props.actions) {
+			// upgrade older actions
+			if (action.actionId === 'route') {
+				if (action.options.id_dst_var_cbx === undefined) {
+					action.options.id_dst_var_cbx = false
+					result.updatedActions.push(action)
+				}
+				if (action.options.id_src_var_cbx === undefined) {
+					action.options.id_src_var_cbx = false
+					result.updatedActions.push(action)
+				}
+				if (action.options.id_dst_var === undefined) {
+					action.options.id_dst_var = ''
+					result.updatedActions.push(action)
+				}
+				if (action.options.id_src_var === undefined) {
+					action.options.id_src_var = ''
+					result.updatedActions.push(action)
+				}
+			}
+		}
+		// console.log('Upgrading feedbacks:', props.feedbacks)
+		for (const feedback of props.feedbacks) {
+			// upgrade older feedbacks
+			if (feedback.feedbackId === 'output_routing') {
+				if (feedback.options.output_var_cbx === undefined) {
+					feedback.options.output_var_cbx = false
+					result.updatedFeedbacks.push(feedback)
+				}
+				if (feedback.options.input_var_cbx === undefined) {
+					feedback.options.input_var_cbx = false
+					result.updatedFeedbacks.push(feedback)
+				}
+				if (feedback.options.output_var === undefined) {
+					feedback.options.output_var = ''
+					result.updatedFeedbacks.push(feedback)
+				}
+				if (feedback.options.input_var === undefined) {
+					feedback.options.input_var = ''
+					result.updatedFeedbacks.push(feedback)
+				}
+			}
+		}
+		// console.log('Upgraded Config:', JSON.stringify(result.updatedConfig, null, 2))
+		// console.log('Upgraded Actions:', JSON.stringify(result.updatedActions, null, 2))
+		// console.log('Upgraded Feedbacks:', JSON.stringify(result.updatedFeedbacks, null, 2))
 		return result
 	},
 ]
